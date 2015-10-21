14382b98d766c3130aa3f48431b3af601e4e7aee
existenzial <ripley36706@gmail.com>
2015-10-20 17:34:35 -0700

    "Messages Finally Show Up"

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,10 +2,11 @@
 var app = {};
 
 app.init = function(){ 
+  app.server = 'https://api.parse.com/1/classes/chatterbox';
   app.fetch();
   app.addFriend();
   app.handleSubmit();
-
+  app.displayMessages();
 };
 app.send = function(message){
   $.ajax({
@@ -23,18 +24,62 @@ app.send = function(message){
 };
 
 app.fetch = function(){
+  var response = '';
   $.ajax({
     type:"GET",
-    url: this.url,
-    data: JSON.stringify(),
-    success: function(){
+    url: this.server,
+    data: {order: '-createdAt'},
+    contentType: 'application/json',
+    success: function(text){
       console.log("Successful retrieval")
+      response = text.results;
+      console.log('response', response);
+      app.displayMessages(response);
+      // console.log('text', text);
+
     },
     error: function(){
       console.log("Sorry, try again")
     }
+  //append messages to #chats.
+    // $(response).appendTo('#chats');
   });
+    console.log('response', response);
 };
+
+app.userPost = function(message){
+  var $username = $('<div>').addClass('username').text(message.username); 
+  var $postText = $('<div>').addClass('postText').text(message.text); 
+  var $roomname = $('<div>').addClass('roomname').text(message.roomname);
+  var $message = $('<div>').addClass('postChat').attr('data-id', message.objectId).append($username, $postText, $roomname);
+
+  return $message; 
+};
+
+app.handleMessage = function(message){
+  var $post = app.userPost(message);
+  $('#chats').prepend($post);
+};
+
+app.displayMessages = function(messages){
+  // messages = this.messages;
+    // console.log('messages', messages);
+  for(var i = 0; i < messages.length; i++) {
+    app.handleMessage(messages[i]);
+     
+  }
+};
+  //username: message.username,
+  //text: message.text,
+  //roomname: message.roomname
+
+// var message = {
+//   username: 'Mel Brooks',
+//   text: 'It\'s good to be the king',
+//   roomname: 'lobby'
+// };
+
+
 
 
 
